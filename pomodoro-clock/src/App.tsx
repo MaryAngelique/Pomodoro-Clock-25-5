5c504e019516dad6206ee1c83afec9535645fa56
Angelique Abacajan <ma_utsey@yahoo.com>
2023-09-01 15:50:20 -0400

    Installed dependencies and implemented variables

--- pomodoro-clock/src/App.tsx
+++ pomodoro-clock/src/App.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const defaultBreakTime = 5 * 60;
+const defaultSessionTime = 25 * 60;
+const min = 60;
+const max = 60 * 60;
+const interval = 60;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
